ca1ef89f6e3fa673dffde5f3a7bf67ee2ab6815f
Anderson Rostirolla <anderson.rostirolla@linx3.com>
2020-11-16 05:20:42 -0300

    feat: adding cors support

--- src/api/routes/omdb.js
+++ src/api/routes/omdb.js
@@ -1,26 +1,30 @@
 const axios = require('axios')
+const cors = require('cors')
 require('dotenv').config()
 
+const corsOptions = {
+  origin: 'http://localhost:8081',
+  optionsSuccessStatus: 200
+}
+
 module.exports = app => {
-  app.route('/search')
-    .get((req, res) => {
-      const queryParams = req.query.q
-      const omdbUrl = `http://www.omdbapi.com/?apikey=${process.env.APIKEY}&s=${queryParams}`
+  app.get('/search', cors(corsOptions), (req, res) => {
+    const queryParams = req.query.q
+    const omdbUrl = `http://www.omdbapi.com/?apikey=${process.env.APIKEY}&s=${queryParams}`
 
-      axios.get(omdbUrl)
-        .then(resp => resp.data)
-        .then(resp => res.status(200).json(resp))
-        .catch(() => {
-          return res.status(404).json({
-            message: 'Failed request from omdb api.'
-          })
+    axios.get(omdbUrl)
+      .then(resp => resp.data)
+      .then(resp => res.status(200).json(resp))
+      .catch(() => {
+        return res.status(404).json({
+          message: 'Failed request from omdb api.'
         })
-    })
-
-  app.route('/*')
-    .get((req, res) => {
-      res.status(200).json({
-        message: 'Access route /search with param ?q= to get results.'
       })
+  })
+
+  app.get('/*', (req, res) => {
+    res.status(200).json({
+      message: 'Access route /search with param ?q= to get results.'
     })
+  })
 }
--- src/config/express.js
+++ src/config/express.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const consign = require('consign')
+const cors = require('cors')
 require('dotenv').config()
 
 module.exports = () => {
   const app = express()
 
   app.set('port', process.env.PORT || 8080)
-
+  app.use(cors())
   app.use(bodyParser.json())
 
   consign({ cwd: 'src/api' })
